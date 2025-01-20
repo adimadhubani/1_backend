7804bc1b8de3e33861e5c81d140649d92cbc4323
Aditya Kumar <adityakumar@Adityas-MacBook-Air-5.local>
2025-01-20 11:25:28 +0530

    backend setup completed with router and controller

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 // import { DB_NAME } from "./constants.js";
 
 import mongoose from "mongoose";
-import app from "./app.js";
+import {app} from "./app.js";
 
 import connectDB from "./db/index.js";
 
